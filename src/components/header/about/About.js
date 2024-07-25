b26cb56429f0b912e2294c7360320882e564d569
fiddle-leaf <shanonps@icloud.com>
2024-07-25 19:45:26 -0400

    Edit about-me text

--- src/components/header/about/About.js
+++ src/components/header/about/About.js
@@ -4,7 +4,8 @@ export default function About(props) {
     return ( <div>
         <div>image here</div>
         <ul>
-            <li>shanon. 27. green-thumb cancer peruana in the dmv.</li>
+            <li>shanon. 27. </li>
+                <li>green-thumb cancer sign peruana from the dmv.</li>
             <li>proudly learning front-end development since 2022.</li>
         </ul>
     </div>);
