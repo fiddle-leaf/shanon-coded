dce5edd8ca1b8ae9308117dfa0e5fe2ba18d6218
Shanon Palomino Salinas <ssalinas@cfpboard.org>
2024-07-30 23:55:59 -0400

    Add Grid component to text element

--- src/components/entries/text/Text.js
+++ src/components/entries/text/Text.js
@@ -1,15 +1,17 @@
+import styles from "./Text.module.sass";
+import Grid from "../grid/Grid";
 import Info from "../info/Info";
 
 export default function Text({ title, date, body }) {
   return (
     <article>
-      <div>
-        <h2>{title}</h2>
-        <p>{body}</p>
-      </div>
-      <div>
+      <Grid isRows={false} isWrapped={false}>
+        <div className={styles.text}>
+          <h2>{title}</h2>
+          <p>{body}</p>
+        </div>
         <Info date={date} />
-      </div>
+      </Grid>
     </article>
   );
 }
